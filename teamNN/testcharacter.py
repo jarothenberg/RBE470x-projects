6f307ee6417799a00ef4b56a0a5fcef84b0e5e89
Jack Rothenberg <jack.a.rothenberg@gmail.com>
2024-02-02 17:28:08 -0500

    Continuing work to get monster actions working with probabilities

--- teamNN/testcharacter.py
+++ teamNN/testcharacter.py
@@ -1,4 +1,5 @@
 # This is necessary to find the main code
+from queue import PriorityQueue
 import sys
 sys.path.insert(0, '../bomberman')
 # Import necessary stuff
@@ -36,14 +37,160 @@ class TestCharacter(CharacterEntity):
                                 tile = True
                                 break
         return worldArray
+    
+    def isCellWalkable(self, worldArray, coords):
+        y = coords[0]
+        x = coords[1]
+        if worldArray[y][x] == "W":
+            return False
+        return True
+
+    def walkableNeighbors(self, worldArray, coords):
+        y = coords[0]
+        x = coords[1]
+        neighbors = []
+        if y+1 < len(worldArray):
+            if self.isCellWalkable(worldArray, (y+1, x)):
+                neighbors.append((y+1, x))
+            if x+1 < len(worldArray[0]) and self.isCellWalkable(worldArray, (y+1, x+1)):
+                neighbors.append((y+1, x+1))
+            if x-1 >= 0 and self.isCellWalkable(worldArray, (y+1, x-1)):
+                neighbors.append((y+1, x-1))
+        if y-1 >= 0:
+            if self.isCellWalkable(worldArray, (y-1, x)):
+                neighbors.append((y-1, x))
+            if x+1 < len(worldArray[0]) and self.isCellWalkable(worldArray, (y-1, x+1)):
+                neighbors.append((y-1, x+1))
+            if x-1 >= 0 and self.isCellWalkable(worldArray, (y-1, x-1)):
+                neighbors.append((y-1, x-1))
+        if x+1 < len(worldArray[0]) and self.isCellWalkable(worldArray, (y, x+1)):
+            neighbors.append((y, x+1))
+        if x-1 >= 0 and self.isCellWalkable(worldArray, (y, x-1)):
+            neighbors.append((y, x-1))
+        return neighbors
+    
+    def cost(self, current, next):
+        currentY = current[0]
+        currentX = current[1]
+        nextY = next[0]
+        nextX = next[1]
+
+        distance = (((nextY - currentY)**2)+(nextX - currentX)**2)**0.5
+        return distance
+    
+    def heuristic(self, goalCoords, coords):
+        coordsY = coords[0]
+        coordsX = coords[1]
+        goalCoordsY = goalCoords[0]
+        goalCoordsX = goalCoords[1]
+
+        distance = (((goalCoordsY - coordsY)**2)+(goalCoordsX - coordsX)**2)**0.5
+        return distance
+    
+    def aStar(self, worldArray, startCoords, goalCoords):
+        frontier = PriorityQueue()
+        frontier.put(startCoords, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[startCoords] = None
+        cost_so_far[startCoords] = 0
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == goalCoords:
+                break
+            
+            for next in self.walkableNeighbors(worldArray, current):
+                new_cost = cost_so_far[current] + self.cost(current, next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + self.heuristic(goalCoords, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+
+        step = goalCoords
+        finalPath = [step]
+        while finalPath[0] != startCoords:
+            finalPath.insert(0, came_from[step])
+            step = came_from[step]
+        return finalPath
 
     def evalState(self, world):
         stateScore = 0
         stateProb = 0
         return [stateScore, stateProb]
 
-    def calcMoveProb(self, world):
-        prob = 0
+    def calcMoveProb(self, world, monCoords, playerCoords):
+        towardsPlayer = 0.2 # 3 cells facing player = 60%
+        awayFromPlayer = 0.05 # 3 cells away from player = 15%
+        sideFromPlayer = 0.1 # 2 cells side to player = 20%
+        noMove = 0.05 # 1 cell no moving = 5%
+        path = self.aStar(world, monCoords, playerCoords)
+        dy = path[1][0] - path[0][0]
+        dx = path[1][1] - path[0][1]
+        prob = np.zeros((9,9))
+
+        prob[1][1] = noMove
+        if dy == -1:
+            prob[0][1] = towardsPlayer
+            if dx == 1:
+                prob[0][2] = towardsPlayer
+                prob[1][2] = towardsPlayer
+                prob[0][0] = sideFromPlayer
+                prob[2][2] = sideFromPlayer
+                prob[1][0] = awayFromPlayer
+                prob[2][0] = awayFromPlayer
+                prob[2][1] = awayFromPlayer
+            elif dx == -1:
+                prob[0][0] = towardsPlayer
+                prob[1][0] = towardsPlayer
+                prob[0][2] = sideFromPlayer
+                prob[2][0] = sideFromPlayer
+                prob[2][1] = awayFromPlayer
+                prob[2][2] = awayFromPlayer
+                prob[1][2] = awayFromPlayer
+            else:
+                prob[0][:] = towardsPlayer
+                prob[1][0] = sideFromPlayer
+                prob[1][2] = sideFromPlayer
+                prob[2][:] = awayFromPlayer
+        elif dy == 1:
+            prob[2][1] = towardsPlayer
+            if dx == 1:
+                prob[2][2] = towardsPlayer
+                prob[1][2] = towardsPlayer
+                prob[0][2] = sideFromPlayer
+                prob[2][0] = sideFromPlayer
+                prob[0][0] = awayFromPlayer
+                prob[1][0] = awayFromPlayer
+                prob[0][1] = awayFromPlayer
+            elif dx == -1:
+                prob[2][0] = towardsPlayer
+                prob[1][0] = towardsPlayer
+                prob[0][0] = sideFromPlayer
+                prob[2][2] = sideFromPlayer
+                prob[0][1] = awayFromPlayer
+                prob[0][2] = awayFromPlayer
+                prob[1][2] = awayFromPlayer
+            else:
+                prob[2][:] = towardsPlayer
+                prob[1][0] = sideFromPlayer
+                prob[1][2] = sideFromPlayer
+                prob[0][:] = awayFromPlayer
+        else:
+            prob[0][1] = sideFromPlayer
+            prob[2][1] = sideFromPlayer
+            if dx == 1:
+                prob[:][2] = towardsPlayer
+                prob[:][0] = awayFromPlayer
+            elif dx == -1:
+                prob[:][0] = towardsPlayer
+                prob[:][2] = awayFromPlayer
+            else:
+                #don't think this is possible
+                pass
+
         return prob
 
     def validActions(self, world):
@@ -80,16 +227,20 @@ class TestCharacter(CharacterEntity):
         maxActEval = 0
         bestAct = 0
 
+        monActions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,0),(0,1),(1,-1),(1,0),(1,1)]
+
         for act in actions:
             actEval = 0
-            for monActs in allMonActions:
-                sensedWorldTemp = world.from_world(sensedWorld)
-                self.doAct(sensedWorldTemp, act)
-                for monAct in monActs:
-                    self.doAct(sensedWorldTemp, monAct)
-                eval = self.evalState(sensedWorldTemp)
-                prob = self.calcMoveProb(sensedWorldTemp, monActs)
-                actEval += eval*prob
+            for mon in monsters:
+                for monActs in monActions:
+                    print(monActs)
+                    sensedWorldTemp = world.from_world(sensedWorld)
+                    self.doAct(sensedWorldTemp, act)
+                    for monAct in monActs:
+                        self.doAct(sensedWorldTemp, monAct)
+                    eval = self.evalState(sensedWorldTemp)
+                    prob = self.calcMoveProb(sensedWorldTemp, monActs)
+                    actEval += eval*prob
             if actEval > maxActEval:
                 maxActEval = actEval
                 bestAct = act
