42f56133ec415a74f3d64f35812e6c99071f0054
AnatigerWP <99768379+AnatigerWP@users.noreply.github.com>
2024-02-05 18:25:58 -0500

    Ana Put her functions :)

--- teamNN/testcharacter.py
+++ teamNN/testcharacter.py
@@ -117,10 +117,52 @@ class TestCharacter(CharacterEntity):
             step = came_from[step]
         return finalPath
 
-    def evalState(self, world):
-        stateScore = 0
-        stateProb = 0
-        return [stateScore, stateProb]
+    def bomberManCoords(self, world):
+        bombMan = []
+        for x in range(world.width()):
+            for y in range(world.height()):
+                if world.characters_at(x,y):
+                    bombMan = [x, y]
+
+        return bombMan
+    
+    def exitCoords(self, world):
+        exitC = []
+        for x in range(world.width()):
+            for y in range(world.height()):
+                if world.exit_at(x,y):
+                    exitC = [x, y]
+
+        return exitC 
+
+
+
+    def evalState(self, world): #this will return a "score" which will tell you how desireable the state is
+        stateScore = 0  #return score
+        #stateProb = 0
+        #return [stateScore, stateProb]
+        coordsBM = self.bomberManCoords(world) #coords for BomberMan
+        monsterLocs = self.getMonCoords(world) # coords for the monter
+        exitLoc = self.exitCoords(world) # coords for exit
+        worldScore = world.scores[self] #is a dictionary { character_name : score } that contains the score of every character in this case getting the score of the character and intergrating it to the function
+        
+        #weights for score change based on values :)
+        monsterDistWeight = -0.2
+        exitDisWeight = -0.1
+
+        #getting score for monsters
+        for monsterLoc in monsterLocs:
+            #calculating the Euclidean between the monster and BM
+            disMonToBMan = math.sqrt((monsterLoc[0]- coordsBM[0])**2 + (monsterLoc[1]- coordsBM[1])**2 ) #distance from BM to Monster
+            stateScore += disMonToBMan * monsterDistWeight
+
+        #getting score from BM to exit (#calculating the Euclidean between the exit and BM)
+        exitToBM =  math.sqrt((exitLoc[0]- coordsBM[0])**2 + (exitLoc[1]- coordsBM[1])**2 )
+        stateScore += exitToBM * exitDisWeight
+
+        #will add score for bombs later.............
+
+        return stateScore
 
     def calcMoveProb(self, world, monCoords, playerCoords):
         towardsPlayer = 0.2 # 3 cells facing player = 60%
